7f89fe95c1bcf8c71b43dcdef5ebf09533a065a3
Lucas Henrique <luc.ucas388@gmail.com>
2019-12-04 11:49:52 -0300

    Aula18e19

--- Aula18/exercicio6.py
+++ Aula18/exercicio6.py
@@ -22,24 +22,40 @@ def ler():
         lista_pessoas.append(pessoas)
     arquivo.close()
     return lista_pessoas
-def salvar(lista_pessoas):
-    lista_pessoas=ler()
-    lista_homem=[]
-    lista_mulher=[]
-    for l in lista_pessoas:
-        if l['sexo'] =='f':
-            lista_mulher.append(lista_pessoas)
-            mulher = open ('Aula18/mulheres.txt','w')
-            mulher.write(lista_mulher[0])
-            mulher.close
-        else:
-            lista_homem.append(lista_pessoas)
-            homem = open ('Aula18/homens.txt','w')
-            homem.write(lista_homem[0])
-            homem.close
-lista_pessoas=ler()
-salvar(lista_pessoas)
 
 
+def lista_festa(lista_entrada):
+    lista_mulheres=[]
+    lista_homens=[]
+    for pessoa in lista_entrada:
+        if int(pessoa['idade'])>=18:
+            if pessoa['sexo'] =='f':
+              lista_mulheres.append(pessoa)  
+            else:
+                lista_homens.append(pessoa)
+    salvar(lista_homens,'homens')
+    salvar(lista_mulheres,'mulheres')
+def salvar(lista,nome):
+    arquivo = open(f'Aula18/{nome}.txt','a')
+    for pessoa in lista:
+        texto = f"{pessoa['codigo']};{pessoa['nome']};{pessoa['sexo']};{pessoa['idade']};"
+        arquivo.write(texto)
+    arquivo.close()
 
+def consulta(lista_consulta_funcao,numero):
+    for lista_consulta in lista_consulta_funcao:
+        if int(lista_consulta['codigo']) == numero:
+            if int(lista_consulta['idade']) >=18:
+                if lista_consulta['sexo']=='f':
+                    print(f"Nome:{lista_consulta['nome']} valor ingresso:R$15,00")
+                else:
+                    print(f"Nome:{lista_consulta['nome']} valor ingresso:R$35,00")
+            else:
+                print('Não pode entrar!')
 
+lista1=ler()
+lista_festa(lista1)
+
+while True:
+    a=int(input('Digite um número'))
+    consulta(lista1,a)
