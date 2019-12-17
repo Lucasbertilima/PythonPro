03a89383a25a0a4c13a375a0d7ce482afd576aa3
Lucas Henrique <luc.ucas388@gmail.com>
2019-12-17 11:36:24 -0300

    Aula28

--- Aula27/exercicio3.py
+++ Aula27/exercicio3.py
@@ -100,39 +100,38 @@ lista_aninhada.insert(2,Nomes)
 lista_aninhada2[3].insert(1,'Pedro Pedroca')
 lista_aninhada.insert(1,'um, dois')
 lista_aninhada2.insert(1,'Adiciono qualquer coisa em qualquer lugar nesta lista!')
-print(lista_aninhada)
-print(lista_aninhada2)
+
 
 # 8) Com a lista1, ordene os números de maior para menor!
 
-lista1 = lista_simples_int(100)
-lista1.sort(reverse=True)
+# lista1 = lista_simples_int(100)
+# lista1.sort(reverse=True)
 
 
 
 # 9) Com a lista2, ordene os números de menor para maior!
 
-lista2 = lista_simples_int(100)
-lista2.sort(reverse=False)
+# lista2 = lista_simples_int(100)
+# lista2.sort(reverse=False)
 
 # 10) Usando o metodo, adicione a lista1 e lista2 (já ordenadas) na lista0.
 
-lista0 = []
-lista0.append(lista1)
-lista0.append(lista2)
+# lista0 = []
+# lista0.append(lista1)
+# lista0.append(lista2)
 
 # 11) Ordene a lista0 e diga qual é o maior valor, menor valor e em quais das listas (lista1 ou lista2)
 # estes pertencem.
-lista0.sort()
-if max(lista0[0]) > max(lista0[1]):
-    print('O maior valor está na lista0[0]')
-elif max(lista0[1]) > max(lista0[0]):
-    print('O maior valor está na lista0[1]')
+# lista0.sort()
+# if max(lista0[0]) > max(lista0[1]):
+#     print('O maior valor está na lista0[0]')
+# elif max(lista0[1]) > max(lista0[0]):
+#     print('O maior valor está na lista0[1]')
 
-if min(lista0[0]) < min(lista0[1]):
-    print('O menor valor está na lista0[0]')
-elif min(lista0[1]) < min(lista0[0]):
-    print('O menor valor está na lista0[1]')
+# if min(lista0[0]) < min(lista0[1]):
+#     print('O menor valor está na lista0[0]')
+# elif min(lista0[1]) < min(lista0[0]):
+#     print('O menor valor está na lista0[1]')
 
 
 # 12) Com a lista_aninhada e lista_aninhada2, do exercicio 7, remova todas as alterações que nelas foram
@@ -140,10 +139,15 @@ elif min(lista0[1]) < min(lista0[0]):
 # usando o f-string (use o .pop() )
 # print(lista_aninhada)
 # print(lista_aninhada2)
-# lista_aninhada[5][2][1].pop()
-# lista_aninhada.pop(1)
-# lista_aninhada2.pop(1)
+lista_aninhada[5][2][1].pop()
+lista_aninhada.pop(1)
+lista_aninhada.pop(3)
+lista_aninhada.pop(2)
+lista_aninhada2.pop(1)
+lista_aninhada2[3].pop(1)
+lista_aninhada2[3][2].pop(3)
 # print(lista_aninhada)
+# print(lista_aninhada2)
 
 # 13) Remova, usando o .remove(), os seguintes itens destas listas:
 # 13.1) cpf da lista_cadastro
@@ -157,16 +161,31 @@ elif min(lista0[1]) < min(lista0[0]):
 # 13.9) cidade
 # 13.10) data_de_nascimento
 
+
 lista_cadastro = ['codigo', 'cpf', 'nome_completo', 'data_de_nascimento',
                   'estado', 'cidade', 'cep', 'bairro', 'rua', 'numero', 'complemento']
+lista_cadastro.remove('cpf')
+lista_cadastro.remove('cidade')
+lista_cadastro.remove('data_de_nascimento')
+
+print(lista_cadastro)
 
 lista_cadastrados = ['1', '11111111111', 'João Carlos', '12/12/90',
                   'SC', 'Camboriú', '8833', 'Tabuleiro', 'Cerejeiras', '45', 'ap 101']
+lista_cadastrados.remove('Camboriú')
+lista_cadastrados.remove('Cerejeiras')
+lista_cadastrados.remove('8833')
+lista_cadastrados.remove('João Carlos')
+lista_cadastrados.remove('11111111111')
+
+print(lista_cadastrados)
 
 lista_cadastrados1 = ['2', '22222222222', 'Paulo Roberto', '23/01/89',
                   'SC', 'Blumenau', '99999', 'Velha', '7 de setembro', '55', '']
+lista_cadastrados1.remove('Paulo Roberto')
+lista_cadastrados1.remove('Velha')
 
-
+print(lista_cadastrados1)
 
 # 14) Com a lista_fusao mostre com f-strig e o metodo .index() a posição dos seguintes elementos:
 # 14.1) cidade
@@ -185,6 +204,24 @@ lista_fusao = ['codigo', 'cpf', 'nome_completo', 'data_de_nascimento',
                   'SC', 'Camboriú', '8833', 'Tabuleiro', 'Cerejeiras', '45', 'ap 101',
                   '2', '22222222222', 'Paulo Roberto', '23/01/89',
                   'SC', 'Blumenau', '99999', 'Velha', '7 de setembro', '55', '']
+a=lista_fusao.index('cidade',0,32)
+b=lista_fusao.index('João Carlos',0,32)
+c=lista_fusao.index('Camboriú',0,32)
+d=lista_fusao.index('12/12/90',0,32)
+e=lista_fusao.index('99999',0,32)
+f=lista_fusao.index('nome_completo',0,32)
+g=lista_fusao.index('22222222222',0,32)
+h=lista_fusao.index('Tabuleiro',0,32)
+i=lista_fusao.index('numero',0,32)
+print(f'Posição de cidade {a}')
+print(f'Posição de João Carlos {b}')
+print(f'Posição de Camboriú {c}')
+print(f'Posição de 12/12/90 {d}')
+print(f'Posição de 99999 {e}')
+print(f'Posição de nome_completo {f}')
+print(f'Posição de 22222222222 {g}')
+print(f'Posição de Tabuleiro {h}')
+print(f'Posição de numero {i}')
 
 
 # 15) Usando o metodo .index(), Crie uma função que localize a posição dos seguintes nomes: 
@@ -202,28 +239,61 @@ lista_fusao = ['codigo', 'cpf', 'nome_completo', 'data_de_nascimento',
 
 
 lista_cadastros = [['1', 'Arnaldo', '23', 'm', 'alexcabeludo2@hotmail.com', '014908648117'], ['2', 'Haroldo', '44', 'f', 'baratarebelde@gmail.com', '050923172729'], ['3', 'Pilar', '50', 'm', 'wanderson10sp@gmail.com', '018937341049'], ['4', 'Suzete Salvador', '45', 'f', 'eladiomp2@yahoo.com.br', '056928409823'], ['5', 'Riane', '37', 'f', 'orkutzimpower@terra.com.br', '018916004377'], ['6', 'Waldir', '34', 'f', 'nandah.s2@bol.com.br', '058903756441'], ['7', 'Lilian', '22', 'f', 'arydoido@gmail.com', '031958621596'], ['8', 'Matilde', '20', 'm', 'eu_kaka_@hotmail.com', '012941959390'], ['9', 'Samanta', '19', 'm', 'carro.tuning@yahoo.com.br', '028964480437'], ['10', 'Margarida', '30', 'm', 'paraaconta.08@hotmail.com', '047903547580'], ['11', 'Evelyn', '31', 'm', 'joaosilvaticudo@gmail.com', '053958638386'], ['12', 'Alessio', '29', 'm', 'w.nill02@gmail.com', '033961294774'], ['13', 'Yolanda', '25', 'm', 'patty_karen2005@hotmail.com', '027903312626'], ['14', 'Germana', '33', 'f', 'jarlinhatopdelinhagv@hotmail.com', '053964603415'], ['15', 'Helio', '33', 'f', 'juh.slim@gmail.com', '046997316461'], ['16', 'Liége', '21', 'f', 'gledsonlds@hotmail.com', '056992948431'], ['17', 'Yan', '42', 'm', 'lucapratto@yahoo.com.br', '016963562866'], ['18', 'Silvain', '50', 'f', 'hie.s2@hotmail.com', '021963399433'], ['19', 'Brian', '33', 'f', 'juliagabrielle06@hotmail.com', '027962676732'], ['20', 'Deoclides', '40', 'f', 'patriciamascena@gmail.com', '012961047979'], ['21', 'Jaqueline', '32', 'm', 'aninha183@hotmail.com', '014958997782'], ['22', 'Rosamaria', '45', 'f', 'j_leosao@hotmail.com', '026944672627'], ['23', 'Carla', '42', 'm', 'jhasdfjo@hotmail.com', '046976625208'], ['24', 'Aida Santos', '30', 'f', 'nayara.cristinap@hotmail.com', '034920819199'], ['25', 'Thomas', '19', 'm', 'jfdslinda@bol.com.br', '030974027667'], ['26', 'Naiara', '23', 'm', 'darknees_666@ig.com.br', '018976696717'], ['27', 'Karyne', '17', 'm', 'garotosonhador_1@hotmail.com', '054984689319'], ['28', 'Alenis Dias', '43', 'f', 'vi_vi_cristinaf@hotmail.com', '034980886309'], ['29', 'Grace', '38', 'm', 'amandakell@uol.com.br', '041932906720'], ['30', 'Zacarias', '31', 'm', 'loca.som@hotmail.com', '041926007066']]
-
+def localiza(lista_cadastros,nome):
+    lista = lista_cadastros
+    nomes=nome
+    for i in nomes:
+        for j in range(0,30):
+            try:
+                a = lista[j].index(i,0,6)
+            except ValueError:
+                pass
+            else:
+                print(f'A posição do nome {i} é {a+1} na lista {j+1}')
+nome = ['Germana', 'Deoclides', 'Zacarias', 'Karyne', 'Helio', 'Silvain', 'Aida Santos']
+# localiza(lista_cadastros,nome)
 
 # 16) Conte na lista1 a quantidade dos seguintes valores (use o f-string):
+lista1 = lista_simples_int(10000)
 # 16.1) 4529
+print(f'4529 aparece {lista1.count(4529)} vezes')
 # 16.2) 29
+print(f'29 aparece {lista1.count(29)} vezes')
 # 16.3) 1107
+print(f'1107 aparece {lista1.count(1107)} vezes')
 # 16.4) 7927
+print(f'7927 aparece {lista1.count(7927)} vezes')
 # 16.5) 6967
+print(f'6967 aparece {lista1.count(6967)} vezes')
 # 16.6) 5964
+print(f'5964 aparece {lista1.count(5964)} vezes')
 # 16.7) 8893
+print(f'8893 aparece {lista1.count(8893)} vezes')
 # 16.8) 3972
+print(f'3972 aparece {lista1.count(3972)} vezes')
 # 16.9) 10
+print(f'10 aparece {lista1.count(10)} vezes')
 # 16.10) 8548
+print(f'8548 aparece {lista1.count(8548)} vezes')
 # 16.11) 8214
+print(f'8214 aparece {lista1.count(8214)} vezes')
 # 16.12) 169
+print(f'169 aparece {lista1.count(169)} vezes')
 # 16.13) 6214
+print(f'6214 aparece {lista1.count(6214)} vezes')
 # 16.14) 15
+print(f'15 aparece {lista1.count(15)} vezes')
 # 16.15) 4937
+print(f'4937 aparece {lista1.count(4937)} vezes')
 # 16.16) 9909
+print(f'9909 aparece {lista1.count(9909)} vezes')
 # 16.17) 3412
+print(f'3412 aparece {lista1.count(3412)} vezes')
 # 16.18) 6306
+print(f'6306 aparece {lista1.count(6306)} vezes')
 # 16.19) 306
+print(f'306 aparece {lista1.count(306)} vezes')
+
 
 lista1 = lista_simples_int(10000)
 
@@ -236,8 +306,17 @@ lista1 = lista_simples_int(10000)
 
 
 listabin = binario()
+def converte(listabin):
+    listabin = listabin
+    lista=[]
+    for numeros in listabin:
+        numero = int(numeros)
+        lista.append(numero)
+    return lista
 
-
+numero = converte(listabin)
+print(listabin)
+print(numero.reverse())
 # 18) com as seguintes listas, imprima elas e .reverte() suas posições. Some as posições e retorne a lista com
 # as somas.
 # Exemplo:
@@ -247,7 +326,9 @@ listabin = binario()
 
 # 18.1) lista1
 lista1 = lista_simples_int(8)
-
+print(lista1)
+p=lista1.reverse()
+print(p)
 # 18.2) lista2
 lista2 = lista_simples_int(8)
 
---
+++ Aula28/exercicio4.py
@@ -0,0 +1,112 @@
+# Aula 28 - 17-12-2019
+# Listas
+
+# DICA!!!!
+# Procurem primeiro o padrão que estas listas vão apresentar! Depois de encontrado, faça o uso da linguagem
+# para facilitar na hora de codar!
+
+
+lista1 = [['frutas','verduras','legumes','preço'],
+         ['mamão','abacaxi','laranja','uva','pera','maçã','vergamota'],
+         ['alface crespa', 'alface lisa','rucula','almerão','repolho','salsinha','cebolinha'],
+         ['feijão', 'erviha', 'lentilha','vagem','feijão branco','gão de bico','soja'],
+         [ [10.00, 2.56, 5.25, 9.5, 10.05, 15, 5.75], [2.99, 2.95, 3.5, 3.25, 5.89, 2.9, 2.5],
+           [9.0, 5.0, 7.5, 1.75, 10.9, 5.99, 3.55] 
+         ]
+        ]
+
+
+lista2 = ['nome',   ['Alex'   ,'Paulo'  ,'Pedro'  ,'Mateus' ,'Carlos' ,'João'   ,'Joaquim'],
+                'telefone',['4799991','4799992','4799993','4799994','4799995','4799996','4799997'],
+                'email',   ['a@a.com','b@b.com','c@c.com','d@d.com','e@e.com','f@f.com','g@g.com'],
+                'idade',   ['18'     ,'25'     ,'40'     ,'16'     ,'15'     ,'19'     ,'17'     ]
+                ]
+
+# 1) Faça um dicionário com a lista1 onde cada elemento esteja junto com o seu respectivo
+# preço. (Dica: Use a indexação e fatiamento para ajudar)
+tipo = lista1[0]
+lista_frutas=[]
+lista_verduras=[]
+lista_legumes=[]
+for i in range(0,7):
+    dicio = {lista1[0][0]:lista1[1][i],lista1[0][3]:lista1[4][0][i]}
+    lista_frutas.append(dicio)
+    dicio = {lista1[0][1]:lista1[2][i],lista1[0][3]:lista1[4][1][i]}
+    lista_verduras.append(dicio)
+    dicio = {lista1[0][2]:lista1[3][i],lista1[0][3]:lista1[4][2][i]}
+    lista_legumes.append(dicio)
+
+print(lista_frutas)
+print(lista_legumes)
+print(lista_verduras)
+# 2) Com o dicionário, imprima os seguintes valores:
+# 2.1) Preço do feijão
+print(lista_legumes[0])
+
+# 2.2) Preço da cebolinha
+print(lista_verduras[5])
+# 2.3) Preço da Alface lisa
+print(lista_verduras[1])
+# 2.4) Preço do Abacaxi
+print(lista_frutas[1])
+# 2.5) Preço do feijão branco.
+print(lista_legumes[4])
+# 3) Com a lista1 qual seria a média dos valores das frutas, verduras e legumes?
+print(f'Soma frutas {sum(lista1[4][0])}')
+print(f'Media frutas {sum(lista1[4][0])/len(lista1[4][0])}')
+
+print(f'Soma frutas {sum(lista1[4][1])}')
+print(f'Media frutas {sum(lista1[4][1])/len(lista1[4][1])}')
+
+print(f'Soma frutas {sum(lista1[4][2])}')
+print(f'Media frutas {sum(lista1[4][2])/len(lista1[4][2])}')
+# 4) Com a lista 1, Qual é o maior e menor valor das frutas, verduras e legumes?
+print(f'O menor valor das frutas é {min(lista1[4][0])}')
+print(f'O maior valor das frutas é {max(lista1[4][0])}')
+
+print(f'O menor valor dos legumes é {min(lista1[4][1])}')
+print(f'O maior valor dos legumes é {max(lista1[4][1])}')
+
+print(f'O menor valor das verduras é {min(lista1[4][2])}')
+print(f'O maior valor das verduras é {max(lista1[4][2])}')
+
+# 5) Adicione no cabeçalho da lista1 (entre o legumes e preço) a "roupa". Aṕos adicione de forma que fique 
+# com a lista coerente 7 roupas e os seus preços.
+
+lista1[0].insert(3,'roupa')
+print(lista1)
+roupas = ['roupa1','roupa2','roupa3','roupa4','roupa5','roupa6','roupa7']
+preco = [50.0,67.24,190.50,99.99,67.00,45.54,250.50]
+lista1.insert(4,roupas)
+lista1[5].insert(3,preco)
+print(lista1)
+# 6) Salve esta lista em um arquivo .txt de moque que cada linha tenha o item e seu preço. 
+
+# 7) Com a lista2, crie uma lista com dicionário onde cada dicionário é um cadastro de uma pessoa.
+
+# 8) Organize a lista2, retirando o cabeçalho e junte os dados de modo que cada cliente ocupe uma lista. Após, salve os dados
+# em um arquivo .txt 
+
+# 9) Criando uma fila. Uma fila é uma estrutura de dados onde o primeiro item que entra é o ultimo que sai. Resumindo, o item novo
+# entra no indice 0 da lista e sai pelo ultimo indice. 
+# Ex:
+# >>> []
+# >>> [1]
+# >>> [2,1]
+# >>> [3,2,1]
+# >>> [3,2]
+# >>> [3] 
+
+# Crie um programa que adiciona novos clientes em uma fila e conforme vai atendendo, remova-os da fila do caixa da loja.
+
+# 10) Criando uma pilha. Uma pilha é uma estrutura de dados onde o primeiro que entra é o ultimo a sair. Resumindo,
+# Os elementos são adicionados e removidos no mesmo lado da lista.
+# Ex:
+# >>> []
+# >>> [1]
+# >>> [1,2]
+# >>> [1,2,3]
+# >>> [1,2]
+# >>> [1] 
+
+# Crie um programa em que adicione os novos números na pilha. Após some eles removendo da pilha.
