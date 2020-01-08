441d584dfa41cbf6bd995882da6c4d7c15474d50
Lucas <luc.ucas388@gmail.com>
2020-01-07 23:20:58 -0300

    Arquivos

--- aeroporto/exercicio.py
+++ aeroporto/exercicio.py
@@ -2,8 +2,9 @@ aviao = []
 passageiro = []
 vaero = False
 piloto = []
-terminal = ['comissaria1','comissaria2', 'piloto', 'oficial1', 'oficial2', 'chefe de serviço','policial', 'presidiario']
+terminal = []
 def adicinar_motorista(opcao, passeiro, vaero, aviao) :   
+    
     if opcao == '1':
         if vaero == False:
             contador = 0
@@ -11,18 +12,8 @@ def adicinar_motorista(opcao, passeiro, vaero, aviao) :
                 print(contador, i)
                 contador += 1
             motorista = int(input(f'''Quem você gostaria de adicionar?'''))
-            piloto.append(terminal[motorista])
-            if piloto != 'piloto' or piloto != 'chefe de serviço' or piloto != 'policial':
-                print('A pessoa selecionada não pode dirigir o carro')
-                print('Podem dirigir Piloto chefe de serviço e policial')
-                contador = 0
-                for i in terminal:
-                    print(contador, i)
-                    contador += 1
-                motorista = int(input(f'''Quem você gostaria de adicionar?'''))
             piloto.append(terminal.pop(motorista))
-            
-            print(f'{piloto[0]} embarcou no veiculo')
+            print(f'=================\n{piloto[0]} embarcou no veiculo\n=================')
             
         elif vaero == True:
             contador = 0
@@ -31,10 +22,9 @@ def adicinar_motorista(opcao, passeiro, vaero, aviao) :
                 contador += 1
             motorista = int(input(f'Quem você gostaria de adicionar?'))
             piloto.append(aviao[motorista])
-            print(f'{piloto[0]} embarcou no veiculo')
+            print(f'=================\n{piloto[0]} embarcou no veiculo\n=================')
             aviao.remove(aviao[motorista])   
-            
-                
+def adicionar_passageiro(terminal):
     escolha = input('''Deseja adicionar passageiro?
             1 - Sim
             2 - Não''')
@@ -45,53 +35,44 @@ def adicinar_motorista(opcao, passeiro, vaero, aviao) :
             contador += 1
         escolhadnv = int(input('Quem você gostaria de adicionar?'))
         passageiro.append(terminal.pop(escolhadnv))
-        print(f'{passageiro[0]} embarcou no veículo')          
+        print(f'=================\n{passageiro[0]} embarcou no veiculo\n=================')          
         
     elif escolha == '2':
         pass
-    
+def veicuolo_movimento():
+    import time
     print('Veiculo esta andando')
+    for i in range(3):
+        time.sleep(1)
+        print('.')
+        
     
-    
+def desembarque(piloto, passageiro,vaero):   
     if vaero == False:
-        print(f'1 - {piloto}\n2 - {passageiro}\n3 - todos')
-        desembarque = input('Quem vai desembarcar?')
-        if desembarque == '1':
-            aviao.append(piloto.pop(0))
+        aviao.append(piloto.pop(0))
+        print(f'{aviao[-1]} embarcou no avião')
+        if passageiro:
+            aviao.append(passageiro.pop(0))
             print(f'{aviao[-1]} embarcou no avião')
-        elif desembarque == '2':
-            aviao.append(passageiro.pop(0))
-        elif desembarque == '3':
-            aviao.append(piloto.pop(0))
-            aviao.append(passageiro.pop(0))
-        
+    
+        vaero = True
+            
     elif vaero == True:
-        print(f'1 - {piloto}\n2 - {passageiro}\n3 - todos')
-        desembarque = input('Quem vai desembarcar?')
-        if desembarque == '1':
-            terminal.append(piloto.pop(0))
+        terminal.append(piloto.pop(0))
+        print(f'{terminal[-1]} desembarcou no terminal')
+        if passageiro: 
+            terminal.append(passageiro.pop(0))
             print(f'{terminal[-1]} desembarcou no terminal')
-            vaero = False
-        elif desembarque == '2':
-            terminal.append(passageiro.pop(0))
-        elif desembarque == '3':
-            terminal.append(piloto.pop(0))
-            terminal.append(passageiro.pop(0))
         
-    
-    
-    
-
-    
-
-       
         
+        vaero = False
+    return vaero
+    
 
-
-
-
-
-
+arquivo = open('aeroporto/terminal.txt','r')
+for i in arquivo:
+    terminal.append(i)
+arquivo.close()
 while True:
     opcao = input('''##################################HBSIS AIRLINES##################################   
             Digite abaixo oque você gostaria de fazer...
@@ -99,13 +80,38 @@ while True:
             2 - mostrar pessoas do aviao
             3 - mostrar pessoas no terminal
             4 - mostrar quem esta no veiculo
+            5 - Fim 
+##################################HBSIS AIRLINES##################################
     ''')
    
     if opcao == '1':
         adicinar_motorista(opcao, passageiro,vaero, aviao)
+        adicionar_passageiro(terminal)
+        veicuolo_movimento()
+        vaero = desembarque(piloto, passageiro,vaero)
+        
     elif opcao == '2':
         print(aviao)
     elif opcao == '3':
         print(terminal)
     elif opcao == '4':
         print(passageiro, piloto)
+    elif opcao == '5':
+        arquivo = open('aeroporto/aviao.txt','w')
+        for i in aviao:
+            arquivo.write(i)
+            arquivo.write('\n')
+        arquivo.close()
+
+
+    # a = open('Lucas_Matheus\Terminal.txt', 'w') 
+    # for i in terminal:
+    #     a.write(i)
+    #     a.write('\n')
+    # a.close()
+    # b = open('Lucas_Matheus\Aviao.txt', 'w') 
+    # for j in aviao:
+    #     b.write(j)
+    #     b.write('\n')
+    # b.close()
+
