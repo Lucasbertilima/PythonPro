6b288e4bf1513d84316299f3b20a616ccd5d92e8
Lucas Henrique <luc.ucas388@gmail.com>
2020-01-08 11:46:24 -0300

    Aula 31

---
+++ aula31/BancodeDados.py
@@ -0,0 +1,18 @@
+# Banco de dados
+# SGBD - Sistema Gerenciador de Banco de Dados
+# Mysql, SqlServer ...
+
+#Mysql = MariaDb
+
+# CRUD
+# C = CREATE
+# R = READ
+# U = UPDATE
+# D = DELETE
+
+import time 
+from time import sleep
+
+for i in range(3600,-1,-1):
+    print(i)
+    time.sleep(0.0473)
